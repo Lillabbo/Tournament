511885112893f982884f4257e8a6b33e999dc4e3
Cybermanboss <mathias.alm@gmail.com>
2022-10-13 10:57:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,6 @@ import pathlib
 # global configuration
 import config
 
-# DAOs
-import database.user as user
-import database.post as post
-
-# auth
-from auth import protected, new_token, del_token
-
 # pages
 import pages.posts as posts
 import pages.userprofile as profile
@@ -27,14 +20,9 @@ app.static('static/', 'static')
 app.ctx.msg = ""
 
 # Create image folders
-pathlib.Path("static/images/icons").mkdir(parents=True, exist_ok=True)
-pathlib.Path("static/images/posts").mkdir(parents=True, exist_ok=True)
+pathlib.Path("static/").mkdir(parents=True, exist_ok=True)
+pathlib.Path("static/").mkdir(parents=True, exist_ok=True)
 
-# Create database tables
-userDAO = user.UserDAO()
-postDAO = post.PostDAO()
-userDAO.create_table()
-postDAO.create_table()
 
 # Endpoints
 @app.get("/")
@@ -71,64 +59,10 @@ async def edit_profile(request):
     user = userDAO.get_user(request.ctx.username)
     return html(profile.edit_profile(user))
 
-@app.get("/logout")
-@protected
-async def logout(request):
-    """Log the user out."""
-    del_token(request.ctx.username)
-    logger.info(f"Logged out: {request.ctx.username}")
-    return redirect("/")
 
-@app.post("/update_profile")
-@protected
-async def update_profile(request):
-    """Receive and save updated profile information."""
-    user = userDAO.get_user(request.ctx.username)
-    print(request.form)
-    print(request.files)
 
-    if('profile-icon' in request.files and
-       request.files['profile-icon'] and
-       request.files['profile-icon'][0].body):
-        old_filename = user.img_path
-        iconfile = request.files['profile-icon'][0]
-        imgtype = iconfile.type[6:]
-        img_id = str(uuid.uuid4())
-        img_path = f'images/icons/{img_id}.{imgtype}'
-        fname = f'static/{img_path}'
-        with open(fname, 'wb') as f:
-            f.write(iconfile.body)
-        if old_filename is not None:
-            os.remove(f'static/{old_filename}')
-        user.img_path = img_path
 
-    if('description' in request.form and request.form['description']):
-        desc = request.form['description'][0]
-        user.desc = desc
-
-    userDAO.update(user)
-    return redirect("/profile")
-
-@app.post("/post/text")
-@protected
-async def make_post(request):
-    """Receive and store new text post."""
-    if not ('title' in request.form and
-            request.form['title']):
-        return redirect("/write")
-    
-    title = request.form['title'][0]
-    contents = ""
-    if 'contents' in request.form and request.form['contents']:
-        contents = request.form['contents'][0]
-
-    usrid = userDAO.get_user_id(request.ctx.username)
-
-    pst = post.TextPost(usrid, title, datetime.datetime.utcnow(), contents)
-    postDAO.store(pst)
-    return redirect("/")
-
-@app.post("/post/image")
+@app.post("/create_tournaments")
 @protected
 async def make_img_post(request):
     """Receive and store new image post."""
@@ -158,64 +92,6 @@ async def make_img_post(request):
 
     return redirect("/")
 
-@app.post("/login")
-async def login(request):
-    """Log user in, then redirect to front page."""
-    if not ('uname' in request.form and
-            'pword' in request.form and
-            request.form['uname']   and
-            request.form['pword']):
-        app.ctx.msg = "Udfyld alle felter."
-        return redirect("/")
 
-    username = request.form['uname'][0]
-    password = request.form['pword'][0]
-
-    usr = userDAO.get_user(username)
-    if not usr:
-        app.ctx.msg = "Brugernavn findes ikke."
-        return redirect("/")
-
-    k = usr.make_key(password)
-    if k != usr.key:
-        app.ctx.msg = "Forkert adgangskode."
-        return redirect("/")
-
-    tok = new_token(username)
-    resp = redirect("/")
-    resp.cookies['auth'] = username + ':' + tok
-    resp.cookies['auth']['expires'] = datetime.datetime.utcnow() +\
-                                      datetime.timedelta(days=1)
-    return resp
-
-@app.post("/register")
-async def register(request):
-    """Create a new user, then log them in."""
-    if not ('uname'  in request.form and
-            'pword'  in request.form and
-            'rpword' in request.form and
-            request.form['uname']    and
-            request.form['pword']    and
-            request.form['rpword']):
-        app.ctx.msg = "Udfyld alle felter."
-        return redirect("/")
-    
-    username = request.form['uname'][0]
-    password1 = request.form['pword'][0]
-    password2 = request.form['rpword'][0]
-
-    if password1 != password2:
-        app.ctx.msg = "Passwords matcher ikke."
-        return redirect("/")
-
-    try:
-        usr = userDAO.new(username, password1)
-        userDAO.store(usr)
-        logger.info(f"New user: {username}")
-        return await login(request)
-    except user.UserExistsError as err:
-        logger.info(str(err))
-        app.ctx.msg = f"Brugernavnet {username} er optaget."
-        return redirect("/")
 
 app.run(host='localhost', port=8080)

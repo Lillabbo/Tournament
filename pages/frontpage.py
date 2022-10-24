08916e33b8a75d14ff49e47a0d865dd80bf001fa
Cybermanboss <mathias.alm@gmail.com>
2022-10-24 14:19:25 +0200

    klkl

--- pages/frontpage.py
+++ pages/frontpage.py
@@ -4,7 +4,7 @@ from dominate.tags import *
 from sanic import Sanic
 from config import APP_NAME
 
-
+from pages.menu import show_menu
 
 
 
@@ -19,34 +19,8 @@ with doc:
     with form(cls='post-form', enctype='multipart/form-data', method='POST', action='/post/image'):
         with div(cls='post'):
             input_(type='text', cls='title_inp',
-                    name='title',
-                    placeholder='Indtast titel...')
+                name='title',
+                placeholder='Indtast titel...')
             input_(type='file', name='image', accept='image/*')
             input_(type='submit', value='Post', cls='button')
-return doc.render()
-
-def create_page():
-    app = Sanic.get_app(APP_NAME)
-    doc = dominate.document(title=f'{APP_NAME} | Skriv')
-
-    with doc.head:
-        link(rel='stylesheet', href=app.url_for('static',
-                                                name='static',
-                                                filename='style.css'))
-
-    with doc:
-        menu_items = [
-            ('Forside', '/'),
-        ]
-        show_menu(menu_items)
-        with form(cls='post-form', method='POST', action='/post/text'):
-            with div(cls='post'):
-                input_(type='text', cls='title_inp',
-                        name='title',
-                        placeholder='Indtast titel...')
-                textarea(cls='contents_inp',
-                            name='contents',
-                            placeholder='Indtast tekst...')
-            input_(type='submit', value='Post', cls='button')
-
     return doc.render()

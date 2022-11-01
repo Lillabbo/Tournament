c3b6c3407433c7d8042af90c10f3e088347426c2
Cybermanboss <mathias.alm@gmail.com>
2022-11-01 12:35:02 +0100

    div menu

--- pages/menu.py
+++ pages/menu.py
@@ -3,6 +3,5 @@ from dominate.tags import *
 def show_menu(menu_items):
     with ul(cls='menu'):
         for (txt, lnk) in menu_items:
-            with li(cls='menu-item'):
-                with div:
+            with div(cls='menu-item'):
                     a(txt, cls='button', href=lnk)

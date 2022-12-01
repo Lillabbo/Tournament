b2b9d0978792d5b7a2e44af0f2875f67b4fc3c3b
26th-Ara <araara0002@gmail.com>
2022-12-01 09:32:21 +0100

    Form

--- pages/Create_tournament.py
+++ pages/Create_tournament.py
@@ -26,32 +26,37 @@ def show_Tournamets():
                     a(txt, cls='button', href=lnk)
 
 #Indsæt spillerens navn til en dictionary med key
-def enter_names():
-    n= int(input("enter name"))
-    d={}
-    for i in range (n):
-        key=input("enter spiller: ")
-        value= input("enter name")
-        d[key]=value
-    print(d)
+# def enter_names():
+#     n= input("enter name")
+#     d={}
+#     for i in range (n):
+#         key=input("enter spiller: ")
+#         value= input("enter name")
+#         d[key]=value
+#     print(d)
 
-# Opret spillere knap og indsæt navn
-def add_contestant():
-    with ul(cls='Add_contestant'):
-        for (txt, lnk) in add_contestant:
-            with div(cls='Add_contestant'):
-                    a(txt, cls='button', href=lnk)    
-    with form(input)
+# # Opret spillere knap og indsæt navn
+# def add_contestant():
+#     with ul(cls='Add_contestant'):
+#         for (txt, lnk) in add_contestant:
+#             with div(cls='Add_contestant'):
+#                     a(txt, cls='button', href=lnk)    
+#     with form()
 
-# 
+# Create_tournament CSS
 def Create_Tournaments():
     app = Sanic.get_app(APP_NAME)
     doc = dominate.document(title=f'{APP_NAME} | Create_Tournamets')
 
     with doc.head:
-        link(rel='stylesheet', href=app.url_for(
-            
-        ))
+        link(rel='stylesheet', href=app.url_for('static', name="static", filename="style.css"))
     with doc:
         h1("Create_Tournamets", cls='page_header')
-            with div() 
+        with div(cls='SC2 Create Tournaments'):
+            header.header()
+
+            with form(method="POST", action="/newtournament"):
+                input_(type = "text", cls = "textinput", placeholder="Skriv dit navn...", name="name")
+                #Her kan tilføjes flere felter
+                input_(type="team",cls="teaminput",) placeholder="Skriv dit "
+                input_(type="submit", value="Opret turnering", cls="button") 
